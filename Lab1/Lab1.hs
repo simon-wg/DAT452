42d68cb9f55be57e17698746047e958f1794ecad
Simon Westlin Green <simon.green@live.se>
2024-11-05 19:07:21 +0100

    Done?

--- Lab1/Lab1.hs
+++ Lab1/Lab1.hs
@@ -1,14 +1,14 @@
-module Lab1 where
 {- Lab 1
    Date: 5/11/2024
-   Authors: 
-   Lab group: 
+   Authors:
+   Lab group:
  -}
 --------------------------------------------
 power :: Integer -> Integer -> Integer
-power n 0  = 1
-power n k | k < 0     = error "power: negative argument"
-          | otherwise = n * power n (k-1)
+power n 0 = 1
+power n k
+  | k < 0 = error "power: negative argument"
+  | otherwise = n * power n (k - 1)
 
 -- A ------------------------
 -- stepsPower n k gives the number of steps that
@@ -20,31 +20,36 @@ stepsPower n k = k + 1
 -- B -------------------------
 -- power1
 
-power1 n k = product (replicate (fromInteger k) n)
+power1 :: Integer -> Integer -> Integer
+power1 n k | k < 0 = error "power: negative argument"
+power1 n k = product (replicate (fromIntegral k) n)
 
 -- C -------------------------
 -- power2
 
 power2 :: Integer -> Integer -> Integer
 power2 n 0 = 1
-power2 n k | even k     = power2 n (div k 2) * power2 n (div k 2)
-           | otherwise  = n * power2 n (k-1)
-
+power2 n k
+  | k < 0 = error "power: negative argument"
+  | even k = power2 n (div k 2) * power2 n (div k 2)
+  | otherwise = n * power2 n (k - 1)
 
 -- D -------------------------
-{- 
+{-
 
 <Describe your test cases here>
-  [(1,0), (2,2), (2,-1), (12,12)]
-
+  [(1,0), (2,2), (2, 2^20), (12,12)]
  -}
 
--- 
+--
+prop_powers :: Integer -> Integer -> Bool
 prop_powers n k = power n k == power1 n k && power1 n k == power2 n k
 
 --
-powerTest :: Bool
-powerTest = prop_powers 1 0 && prop_powers 2 2 && prop_powers 2 (-1) && prop_powers 12 12
+test_cases = [(1, 0), (2, 2), (2, 2 ^ 20), (12, 12)]
+
+powerTest = [prop_powers n k | (n, k) <- test_cases]
 
 --
-prop_powers' = undefined
+prop_powers' :: Integer -> Integer -> Bool
+prop_powers' n k = power n (abs k) == power1 n (abs k) && power1 n (abs k) == power2 n (abs k)
