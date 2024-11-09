03c5e5fca0177c96b919d8df46689bbb3d61ac17
Heldereim <adam.hellgard@hotmail.se>
2024-11-09 16:08:02 +0100

    Implementation of A0,  A1 and A2

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -5,10 +5,8 @@ import Test.QuickCheck
 
 -- A0 Writing out the sequence
 
-size :: Num a => Hand -> a 
-size Empty = 0 
-size (Add card hand) = 1 + size hand
-
+hand2 = (Add (Card (Numeric 2) Hearts)
+              (Add (Card Jack Spades) Empty))
 
 sizeSteps :: [Integer]
 sizeSteps = [ size hand2
@@ -21,6 +19,35 @@ sizeSteps = [ size hand2
 
 -- A1 Displaying a hand nicely
 
+displayCard :: Card -> String
+displayCard (Card (Numeric i) s) = show i ++ " of " ++ show s
+displayCard (Card r s)           = show r ++ " of " ++ show s 
+
 display :: Hand -> String
 display Empty   = ""
-display Add c h = (rank c) ++ "of" ++ (suit c)
+display (Add c h) = displayCard (c) ++ "\n" ++ display h
+
+-- A2 Calc hand value
+
+valueRank :: Rank -> Integer
+valueRank (Numeric i) = i
+valueRank Ace         = 11
+valueRank r           = 10
+
+initialValue :: Hand -> Integer
+initialValue Empty     = 0
+initialValue (Add c h) = valueRank (rank c) + initialValue h
+
+numberOfAces :: Hand -> Integer
+numberOfAces Empty     = 0
+numberOfAces (Add c h)
+    | rank c == Ace = 1 + numberOfAces h 
+    | otherwise     =     numberOfAces h
+
+value :: Hand -> Integer
+value Empty     = 0
+value (Add c h)
+   | initialValue (Add c h) <= 21 = initialValue (Add c h)
+   | otherwise                = initialValue (Add c h) - ((numberOfAces (Add c h)) * 10)
+   where initVal = initialValue (Add c h)
+        
