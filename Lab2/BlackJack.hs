a66d44be043d2c266c1444d482ce1f38bab07cd8
Simon Westlin Green <simon.green@live.se>
2024-11-11 15:31:22 +0100

    Wrote documentation at the same time as we wrote the functions

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -12,6 +12,8 @@ hand2 =
     (Add (Card Jack Spades) Empty)
 
 sizeSteps :: [Integer]
+{- What we do is essentially count cards one by one,
+and removing the counted card from the hand -}
 sizeSteps =
   [ size hand2,
     size
@@ -27,31 +29,41 @@ sizeSteps =
 
 -- A1 Displaying a hand nicely
 
+{- Takes a card as a parameter, and returns a string
+describing the rank and suit of it. -}
 displayCard :: Card -> String
 displayCard (Card (Numeric i) s) = show i ++ " of " ++ show s
 displayCard (Card r s) = show r ++ " of " ++ show s
 
+{- Takes a hand as a parameter, and returns a string
+describing the rank and suit of all cards in it. -}
 display :: Hand -> String
 display Empty = ""
 display (Add c h) = displayCard c ++ "\n" ++ display h
 
 -- A2 Calc hand value
 
+{- Takes a rank as a parameter, and returns the Integer value of it -}
 valueRank :: Rank -> Integer
 valueRank (Numeric i) = i
 valueRank Ace = 11
 valueRank r = 10
 
+{- Takes a hand as a parameter, and returns the Integer value of it,
+counting aces as 11 -}
 initialValue :: Hand -> Integer
 initialValue Empty = 0
 initialValue (Add c h) = valueRank (rank c) + initialValue h
 
+{- Takes a hand as a parameter, and returns the Integer number of aces in it -}
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
 numberOfAces (Add c h)
   | rank c == Ace = 1 + numberOfAces h
   | otherwise = numberOfAces h
 
+{- Takes a hand as a parameter, and returns the Integer value of it,
+counting aces as 11 or 1 depending on if the player would go bust. -}
 value :: Hand -> Integer
 value Empty = 0
 value (Add c h)
@@ -62,11 +74,15 @@ value (Add c h)
 
 -- A3 Is the player bust?
 
+{- Takes a hand as a parameter
+and returns whether or not it is greater than 21-}
 gameOver :: Hand -> Bool
 gameOver h = value h > 21
 
 -- A4 Winner function
 
+{- Takes two hands as parameters, where the first is the guest,
+and the second the bank. Returns which Player wins. -}
 winner :: Hand -> Hand -> Player
 winner guestHand bankHand
   | gameOver guestHand = Bank
