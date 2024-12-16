88b3dcc0fb67c6cdb1417d2e454a0bcaf552087b
Heldereim <adam.hellgard@hotmail.se>
2024-12-16 22:54:17 +0100

    Gameloop, read added
    Added the gameloop part of the program. Additionally set the QA class to derive the Read class for easier reading from storage

--- Lab5/Lab5.hs
+++ Lab5/Lab5.hs
@@ -1,5 +1,4 @@
 import System.IO
-import Parsing
 
 {--
 A class representing the decision tree.
@@ -8,14 +7,20 @@ containing a question and two other QA:s
 or a Leaf containing an answer.
 --}
 data QA = Question String QA QA | Answer String
-  deriving (Show)
+  deriving (Show,Read)
 
+{--
+Base tree for testing purposes
+--}
 baseTree = Question "Is this person from Europe?" 
               (Question "Is this person a scientist?" (Answer "Marie Curie") (Answer "Queen Elizabeth II")) 
               (Question "Is this person an actor" (Answer "Marilyn Monroe") (Answer "Hillary Clinton"))
 
 -- | Function for traversing the question and answers
--- | in a tree by recursively 
+-- | in a tree by recursively asking a question and 
+-- | and traversing the left QA when answered with 'y'
+-- | or traversing the right QA when answered with 'n'.
+-- | Otherwise recursively calls itself and asks the question again.
 traverseTree :: QA -> IO QA
 traverseTree (Question q qa1 qa2) = do 
     putStr ("\n" ++ q ++ " (y/n) ")
@@ -42,7 +47,7 @@ traverseTree (Answer person) = do
                 putStr "\nYES - I WIN!\n"
                 return (Answer person)
         'n' -> do 
-                putStr ("OK -  you won this time.\n")
+                putStr ("\nOK -  you won this time.\n")
                 putStr ("\nJust Curious: Who was your famous person? ")
                 hFlush stdout
                 newPerson <- getLine
@@ -52,8 +57,26 @@ traverseTree (Answer person) = do
                 newQuestion <- getLine
                 return (Question newQuestion (Answer newPerson) (Answer person))
 
+-- | When given a tree, writes that tree to the "database" file
+-- | for the stored tree
 updateTree :: QA -> IO()
 updateTree tree = do writeFile "question.qa" (show tree)
 
--- getTree :: IO QA
--- getTree = do readFile "question.qa"
+-- | Collects a tree from storage to be used in the game
+getTree :: IO QA
+getTree = do 
+        f <- readFile "question.qa"
+        let qa = read f
+        return qa
+
+{-
+Game loop is as follows:
+        1. Get the tree from storage.
+        2. Traverse the tree and update the tree if necessary
+        3. Store the possibly updated tree.
+-}
+main :: IO()
+main = do 
+        tree <- getTree
+        newTree <- traverseTree tree
+        updateTree newTree
